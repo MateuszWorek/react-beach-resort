71e4b7ee2f18be2afc12f7ab59997dc8e84dcc58
Hyperion1040 <mateusz.worek@gmail.com>
2021-02-07 18:16:31 +0100

    react beach application

---
+++ src/codingSteps.js
@@ -0,0 +1,7 @@
+// RoomFilter.js
+import { useContext } from 'react';
+import { RoomContext } from '../context';
+import Title from '../components/Title';
+
+const context = useContexct(RoomContext);
+console.log(context);
